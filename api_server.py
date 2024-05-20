cb97246f1fe2e5fb354c19dfff3f3a8da7a5db23
shanepro321 <shanefpro@gmail.com>
2024-05-20 12:15:59 +0800

    Add files via upload

---
+++ api_server.py
@@ -0,0 +1,1 @@
+from flask import Flask, request, jsonifyfrom whisperkit.pipelines import WhisperKit, WhisperCpp, WhisperMLXpipe = WhisperKit(whisper_version="openai/whisper-large-v3", out_dir="/Users/shane/PycharmProjects/whisper3/audio.txt")print(pipe("audio.wav"))
